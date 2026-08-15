4890d0a587174fa9b50756a075a33a7fbf0f3624
binhkdc <binhkdc.yotech@gmail.com>
2026-08-15 21:51:36 +0700

    test ba

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function getMetadata(videoUrl, useProxy = true) {
         let commandArgs = [
             `"${ytDlpPath}"`,
             `"${videoUrl}"`,
-            '-f "ba[ext=m4a][abr<=128]/wa[ext=m4a]/wa"',
+            '-f "ba[ext=m4a]/ba/bestaudio/b"',
             '--no-playlist',
             '--skip-download',
             '--dump-single-json',
