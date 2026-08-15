cef2c947bab3c42d02f4783bd07d0ec117ce344d
binhkdc <binhkdc.yotech@gmail.com>
2026-08-15 21:17:19 +0700

    test

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function getMetadata(videoUrl, useProxy = true) {
         // Mảng tham số nguyên bản, không dùng ngoặc đôi thủ công
         let args = [
             videoUrl,
-            '-f', 'ba[ext=m4a][abr<=128]/ba[abr<=128]/wa[ext=m4a]/wa', // Ưu tiên m4a audio stream
+            '-f', 'wa[ext=m4a]/wa', // Ưu tiên m4a audio stream
             '--no-playlist',
             '--skip-download',
             '--dump-single-json',
